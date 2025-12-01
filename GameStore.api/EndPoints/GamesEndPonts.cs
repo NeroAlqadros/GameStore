da3ea86c24024ee9297ebd320d90adcd662faa09
NeroAlqadros <hasan.cakir0305@gmail.com>
2025-12-01 21:08:47 +0300

    implemented handling invalid inputs with minimalapis.extensions packet

--- GameStore.api/EndPoints/GamesEndPonts.cs
+++ GameStore.api/EndPoints/GamesEndPonts.cs
@@ -13,14 +13,16 @@ public static class GamesEndPonts
     new (3,"FIFA 23", "Sports", 69.99m,new DateOnly(2022,9,27))
     ];
 
-    public static WebApplication MapGamesEndPoints(this WebApplication app)
+    public static RouteGroupBuilder MapGamesEndPoints(this WebApplication app)
     {
+        var group = app.MapGroup("games")
+                        .WithParameterValidation();
 
         // GET /games
-        app.MapGet("games", () => games);
+        group.MapGet("/", () => games);
 
         //GET /games/1
-        app.MapGet("games/{id}", (int id) =>
+        group.MapGet("/{id}", (int id) =>
         {
             var game = games.Find(game => game.Id == id);
 
@@ -29,7 +31,7 @@ public static class GamesEndPonts
             .WithName(GetGameEndpointName);
 
         //POST /games
-        app.MapPost("games", (CreateGameDto newGame) =>
+        group.MapPost("/", (CreateGameDto newGame) =>
         {
             GameDto game = new(
              games.Count + 1,
@@ -44,7 +46,7 @@ public static class GamesEndPonts
         });
 
         //PUT /games/1
-        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
         {
             var index = games.FindIndex(game => game.Id == id);
 
@@ -65,13 +67,13 @@ public static class GamesEndPonts
         });
 
         //DELETE /games/4
-        app.MapDelete("games/{id}", (int id) =>
+        group.MapDelete("/{id}", (int id) =>
         {
             games.RemoveAll(game => game.Id == id);
 
             return Results.NoContent();
         });
 
-        return app;
+        return group;
     }
 }
