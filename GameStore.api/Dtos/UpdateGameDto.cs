3cc8a2745cb39a45a9a3591704fb527e3bf0b07b
NeroAlqadros <hasan.cakir0305@gmail.com>
2025-12-01 20:47:30 +0300

    İmplemented crud operatins for games, added updategamedto and creategamedto

---
+++ GameStore.api/Dtos/UpdateGameDto.cs
@@ -0,0 +1,7 @@
+namespace GameStore.api.Dtos;
+
+public record class UpdateGameDto(
+string Name,
+string Genre,
+decimal Price,
+DateOnly ReleaseDate);
--- GameStore.api/obj/Debug/net10.0/GameStore.api.AssemblyInfo.cs
+++ GameStore.api/obj/Debug/net10.0/GameStore.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e73c1fdae0d826ab9161503eb5d0c1a85deaf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
